08a1158df6ff09ad8f953f8f98b65365978e32c9
James Reid <jreidmke@gmail.com>
2020-12-31 12:30:30 -0600

    add/remove cart functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import data from './data.json';
+import { Link } from 'react-router-dom'
+import useCart from './hooks';
 
 function App() {
+  const [cart, addToCart, removeFromCart] = useCart();
+
+  const products = Object.keys(data.products).map(key =>
+    <div key={key} className='product-card'>
+      <h1>{data.products[key].name.toUpperCase()} // In Cart: 0</h1>
+      <img src={data.products[key].image_url} id='product-img' alt='product-image'/>
+      <h3>Price: {data.products[key].price}</h3>
+      <button id='add-cart-btn' onClick={() => addToCart(data.products[key].name)}>Add to cart</button>
+      <button id='remove-cart-btn' onClick={() => removeFromCart(data.products[key].name)}>Remove from cart.</button>
+      <hr/>
+    </div>
+  )
+
+  function printCart() {
+    console.log(cart);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products}
+      <button onClick={printCart}>X</button>
     </div>
   );
 }
---
+++ src/hooks.js
@@ -0,0 +1,23 @@
+import {useState} from 'react';
+
+function useCart() {
+    const [cart, setCart] = useState([]);
+
+    function addToCart(key) {
+        setCart(cart => ([
+            ...cart,
+            key
+        ]));
+    };
+
+    function removeFromCart(key) {
+        const itemIdx = cart.indexOf(key);
+        if(itemIdx === -1) return;
+        cart.splice(itemIdx, 1);
+        setCart([...cart]);
+    };
+
+    return [cart, addToCart, removeFromCart]
+}
+
+export default useCart;
